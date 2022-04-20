bd424b0bd6ee55209bd5a01287ba4faa869d5073
ohzeno <blackroseaaa3@gmail.com>
2022-04-20 15:58:04 +0900

    Add p5sketch example

---
+++ p5sketch/sketch.js
@@ -0,0 +1,23 @@
+console.log("sketch blah");
+
+var s = function (sketch) {
+  sketch.setup = function () {
+    document.body.style["userSelect"] = "none";
+    let h = document.body.clientHeight;
+    let c = sketch.createCanvas(sketch.windowWidth, h);
+    c.position(0, 0);
+    c.style("pointer-events", "none");
+    sketch.clear();
+  };
+
+  sketch.draw = function () {
+    // background(0, 255, 0);
+    sketch.stroke(0);
+    sketch.strokeWeight(4);
+    if (sketch.mouseIsPressed) {
+      sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+    }
+  };
+};
+
+var myp5 = new p5(s);
