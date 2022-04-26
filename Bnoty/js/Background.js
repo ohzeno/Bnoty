07d8b8591eafb50bd63dc7a60d5cd7115b6b8d7b
AlwaysRYU <aliverise@naver.com>
2022-04-26 13:50:41 +0900

    Add Show Capture

--- Bnoty/js/Background.js
+++ Bnoty/js/Background.js
@@ -1,4 +1,6 @@
 
+// 설정
+var global = "undefined" != typeof chrome ? chrome : "undefined" != typeof browser ? browser : void 0; 
 
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
@@ -6,11 +8,12 @@ chrome.runtime.onMessage.addListener(
         console.log("[background] request:" + request.method);
         if (request.method === 'test') {
             // 이곳에다 test 메소드 내용을 작성 
-            
+            test();
             // 응답
-            sendResponse({ farewell: 'contentgoodbye' });
+            sendResponse({ farewell: 'test funcion is called' });
         } else if ( request.method === 'ShowCapture' ){
-            
+            ShowCapture();
+            sendResponse({ farewell: 'contentgoodbye' });
         } else if ( request.method === 'CropCapture' ){
             
         } else if ( request.method === 'FullCapture' ){
@@ -18,5 +21,19 @@ chrome.runtime.onMessage.addListener(
         } else if ( request.method === 'ScrollCapture' ){
             
         }
+});
 
-  });
+function test() {
+    console.log("투마로");
+};
+
+function ShowCapture(){
+    console.log("[Background] ShowCapture")
+    // 함수로 저장
+    global.tabs.captureVisibleTab(function(a) {
+        // 추출한 이미지를 이걸로
+        console.log(a);
+     });
+    // var o = global.extension.getURL("screen.html");
+
+}
